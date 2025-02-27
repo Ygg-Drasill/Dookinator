3846dac2cd95f4606b24a957a3d89082eb9d37fe
CowsenF <50503934+CowsenF@users.noreply.github.com>
2025-02-27 10:46:54 +0100

    Splitted yolo from main

---
+++ yolox/yolo.py
@@ -0,0 +1,40 @@
+from ultralytics import YOLO
+import torch
+
+CONFIDENCE_THRESHOLD = 0.4
+
+def init_yolo():
+    # Ensure PyTorch is using the GPU
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    # Load YOLO model on GPU
+    return YOLO("yolov8n.pt").to(device)
+
+def read_frame(model, frame):
+    # run the YOLO model on the frame
+    detections = model(frame)[0]
+
+    # initialize the list of bounding boxes and confidences
+    results = []
+
+    ######################################
+    # DETECTION
+    ######################################
+
+    # loop over the detections
+    for data in detections.boxes.data.tolist():
+        # extract the confidence (i.e., probability) associated with the prediction
+        confidence = data[4]
+
+        # filter out weak detections by ensuring the
+        # confidence is greater than the minimum confidence
+        if float(confidence) < CONFIDENCE_THRESHOLD:
+            continue
+
+        # if the confidence is greater than the minimum confidence,
+        # get the bounding box and the class id
+        xmin, ymin, xmax, ymax = int(data[0]), int(data[1]), int(data[2]), int(data[3])
+        class_id = int(data[5])
+        # add the bounding box (x, y, w, h), confidence and class id to the results list
+        results.append([[xmin, ymin, xmax - xmin, ymax - ymin], confidence, class_id])
+
+    return results
