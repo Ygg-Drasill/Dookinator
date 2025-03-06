8bddfca093f45519aedfc550566a6ab40c470fd8
Androkless <NO01VU@student.aau.dk>
2025-03-06 15:01:04 +0100

    bugfix

--- tests/Test.py
+++ tests/Test.py
@@ -2,7 +2,7 @@ import Homography as h
 import numpy as np
 
 
-def homographyTest():
+def test_homography():
     cam = np.array([[145, 730], [1350, 1260], [990, 770], [1620, 960], [395, 840], [820, 1020]])
     absolute = np.array([[35.75, 20.16], [35.75, -20.16], [46.75, 9.16], [46.75, -9.16], [35.75, 7.31], [35.75, -7.31]])
 
