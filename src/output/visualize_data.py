b33e6f632c94b7ddb7283653b5dff8d8b6dd5fb1
CowsenF <50503934+CowsenF@users.noreply.github.com>
2025-04-16 11:06:20 +0200

    main in visualize_data.py

--- src/output/visualize_data.py
+++ src/output/visualize_data.py
@@ -1,23 +1,33 @@
+import json
+import os
+import sys
+
 import cv2
 import numpy as np
 import yaml
+import pandas as pd
 
-from src.definitions import CONFIG_PATH
-from src.detectionframe.game_detection_frame import GameDetectionFrame
+from src.definitions import CONFIG_PATH, ROOT_DIR
+from src.detectionframe.ball_detection_frame import BallDetectionFrame
+from src.detectionframe.game_detection_frame import GameDetectionFrame, LastTouch
+from src.detectionframe.player_detection_frame import PlayerDetectionFrame
 
 with open(CONFIG_PATH, 'r') as file:
     config = yaml.safe_load(file)  # Read the file once
     output_jsonl_file_path = config['output_jsonl_relative_file_path']
     real_data_jsonl_file_path = config['real_data_jsonl_relative_file_path']
+    meta_file = config['match']['meta_file']
 
+with open(os.path.join(ROOT_DIR, str(meta_file)), 'r') as f:
+    data = json.load(f)
 
 height = 480
 width = 640
 
-football_field_width = 1920
-football_field_height = 1080
+football_field_width = data["pitchLength"]
+football_field_height = data["pitchWidth"]
 
-#shape settings
+# shape settings
 our_away_color = (255, 255, 0)
 our_home_color = (255, 0, 255)
 real_away_color = (0, 255, 0)
@@ -25,7 +35,14 @@ real_home_color = (0, 0, 255)
 radius = 2
 thickness = 1
 
-blank_image = np.zeros((height,width,3), np.uint8)
+blank_image = np.zeros((height, width, 3), np.uint8)
+
+def main() -> int:
+
+
+
+    return
+
 
 def visualize_data(frame: int):
     our_data = read_football_output(output_jsonl_file_path)
@@ -38,11 +55,47 @@ def visualize_data(frame: int):
 
 def read_football_output(file_path: str) -> GameDetectionFrame:
 
-    
+    json_obj = pd.read_json(path_or_buf=file_path, lines=True)
+    frame = json_obj.iloc[0]
 
+    home_players_raw = frame["home_players"]
 
+    home_players = np.array([
+        PlayerDetectionFrame(
+            player_id=p["player_id"],
+            number=p["number"],
+            xyz=np.array(p["xyz"]),
+            speed=p["speed"],
+            opta_id=p["opta_id"]
+        )
+        for p in home_players_raw
+    ])
 
-    pass
+    away_players_raw = frame["away_players"]
+
+    away_players = np.array([
+        PlayerDetectionFrame(
+            player_id=p["player_id"],
+            number=p["number"],
+            xyz=np.array(p["xyz"]),
+            speed=p["speed"],
+            opta_id=p["opta_id"]
+        )
+        for p in away_players_raw
+    ])
+
+    ball_raw = frame["ball"]
+
+    if ball_raw is not None:
+        ball = BallDetectionFrame(
+            xyz=np.array(ball_raw["xyz"]),
+            speed=ball_raw["speed"]
+        )
+    else:
+        ball = None
+
+    game_detection_frame = GameDetectionFrame(period=0, frame_idx=frame, game_clock=0, wall_clock=0, home_players=home_players, away_players=away_players, ball=ball, live=True, last_touch=LastTouch.Home)
+    return game_detection_frame
 
 def gen_image(our_game_detection_frame: GameDetectionFrame, real_game_detection_frame: GameDetectionFrame) -> np.array:
     image = blank_image.copy()
@@ -69,3 +122,5 @@ def read_players(players: np.ndarray, image: np.ndarray, color: tuple[int, int, 
 
     return new_image
 
+if __name__ == '__main__':
+    sys.exit(main())
