ac34e622ddf2162cc0fc8ce2df9b860063c891b2
GustavHus <113035217+GustavHus@users.noreply.github.com>
2025-05-05 14:59:55 +0200

    Virker nu, spiser to jsonl filer og outputter en samlet score og grapher for den første og sidste frame

--- tests/jsonl_comparison/comparison.py
+++ tests/jsonl_comparison/comparison.py
@@ -1,7 +1,13 @@
 import json
+import os
+import sys
+
 import numpy as np
+from matplotlib import pyplot as plt
 from scipy.optimize import linear_sum_assignment
 from tqdm import tqdm
+
+from src.definitions import ROOT_DIR
 
 
 def extract_positions(frame):
@@ -51,9 +57,70 @@ def compare_files(file1_path, file2_path):
 
     average_score = total_score / frame_count
     return average_score
+
+def extract_team_positions(frame):
+    # Separate home and away team player XY positions
+    home = [player["xyz"][:2] for player in frame.get("homePlayers", [])]
+    away = [player["xyz"][:2] for player in frame.get("awayPlayers", [])]
+    return np.array(home), np.array(away)
+
+def plot_side_by_side(frame1, frame2, title1, title2, supertitle):
+    home1, away1 = extract_team_positions(frame1)
+    home2, away2 = extract_team_positions(frame2)
+
+    fig, axes = plt.subplots(1, 2, figsize=(14, 6))
+    fig.suptitle(supertitle, fontsize=16)
+
+    # Dataset 1
+    axes[0].scatter(*home1.T, c='blue', label='Home', s=60)
+    axes[0].scatter(*away1.T, c='red', label='Away', s=60)
+    axes[0].set_title(title1)
+    axes[0].set_xlim(-60, 60)
+    axes[0].set_ylim(-40, 40)
+    axes[0].set_aspect('equal', 'box')
+    axes[0].grid(True)
+    axes[0].legend()
+
+    # Dataset 2
+    axes[1].scatter(*home2.T, c='blue', label='Home', s=60)
+    axes[1].scatter(*away2.T, c='red', label='Away', s=60)
+    axes[1].set_title(title2)
+    axes[1].set_xlim(-60, 60)
+    axes[1].set_ylim(-40, 40)
+    axes[1].set_aspect('equal', 'box')
+    axes[1].grid(True)
+    axes[1].legend()
+
+    plt.show()
+
+def plot_first_and_last_side_by_side(file1_path, file2_path):
+    with open(file1_path, 'r') as f1, open(file2_path, 'r') as f2:
+        lines1 = f1.readlines()
+        lines2 = f2.readlines()
+
+    first_frame_1 = json.loads(lines1[0])
+    last_frame_1 = json.loads(lines1[-1])
+    first_frame_2 = json.loads(lines2[0])
+    last_frame_2 = json.loads(lines2[-1])
+
+    plot_side_by_side(first_frame_1, first_frame_2,
+                      "Dataset 1 - First Frame", "Dataset 2 - First Frame",
+                      "First Frame Comparison")
+
+    plot_side_by_side(last_frame_1, last_frame_2,
+                      "Dataset 1 - Last Frame", "Dataset 2 - Last Frame",
+                      "Last Frame Comparison")
+
+
 def main():
-    file1 = "comparison_data/file1"
-    file2 = "comparison_data/file2"
+
+    file1 = os.path.join(ROOT_DIR, 'tests', 'jsonl_comparison', 'comparison_data', 'file1.jsonl')
+    file2 = os.path.join(ROOT_DIR, 'tests', 'jsonl_comparison', 'comparison_data', 'file2.jsonl')
+
 
     score = compare_files(file1, file2)
+    plot_first_and_last_side_by_side(file1, file2)
     print(score)
+
+if __name__ == '__main__':
+    sys.exit(main())
